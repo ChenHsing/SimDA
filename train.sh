14e0beda7bbd8c68534d95a5218b8c31d3eedc20
ChenHsing <736198316@qq.com>
2024-05-07 18:45:18 +0800

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,1 +1,1 @@
-CUDA_VISIBLE_DEVICES=2 accelerate launch --num_processes 1 train_simda.py --config="configs/car-turn.yaml"
+CUDA_VISIBLE_DEVICES=0 accelerate launch --num_processes 1 train_simda.py --config="configs/car-turn.yaml"
